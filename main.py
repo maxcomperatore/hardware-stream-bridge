99d1a8aea749f9387c41fcd10e64e344ec0e9689
pyoneerC <maxcomperatutti@gmail.com>
2026-08-05 17:10:18 -0300

    bipluk

--- main.py
+++ main.py
@@ -1369,6 +1369,10 @@ async def terms_page(request: Request):
 async def privacy_page(request: Request):
     return render_template("privacy.html", request)
 
+@app.get("/accessibility", response_class=HTMLResponse)
+async def accessibility_page(request: Request):
+    return render_template("accessibility.html", request)
+
 @app.get("/roadmap", response_class=HTMLResponse)
 async def roadmap_page(request: Request):
     return RedirectResponse(url="/resources", status_code=301)
